950c7fce9ef302e034d94919044160ff029b8248
NaeCqde <50701386+NaeCqde@users.noreply.github.com>
2023-05-25 03:34:11 +0900

    init

---
+++ src/lib/twemoji.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let emoji: string;
+    export let type: "svg" | "png" = "svg";
+    export let exClass: string = "";
+    export let exStyle: string = "width: 32px; height: 32px;";
+    export let baseURL = "https://twemoji.naecqde.workers.dev/twemoji/";
+
+    const url = `${baseURL}${emoji}.${type}`;
+</script>
+
+<img class={exClass} src={url} alt={emoji} style={exStyle} />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Twemoji from "$lib/twemoji.svelte";
+</script>
+
+<h1>Welcome to <Twemoji emoji="❤" /> project</h1>
